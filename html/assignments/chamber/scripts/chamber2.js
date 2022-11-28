74a35ee72cd9ade1a6c13a867a7722448452ee57
bashashhazbaz <ash16016@byui.edu>
2022-11-27 23:01:45 -0600

    week12.3

--- html/assignments/chamber/scripts/chamber2.js
+++ html/assignments/chamber/scripts/chamber2.js
@@ -94,18 +94,18 @@ else {
 
 
 //business card stuff
-const requestURL = 'https://github.com/bashashhazbaz/bashashhazbaz.github.io/blob/main/html/assignments/chamber/json/data2.json';
-const cards = document.querySelector('.cards');
+//const requestURL = 'https://github.com/bashashhazbaz/bashashhazbaz.github.io/blob/main/html/assignments/chamber/json/data2.json';
+//const cards = document.querySelector('.cards');
 
-async function getBusiness() {
-    const response = await fetch(requestURL);
-    const data = await response.json();
+//async function getBusiness() {
+  //  const response = await fetch(requestURL);
+  //  const data = await response.json();
     //const prophets = jsonObject['prophets']; 
-    data.business.forEach(business => { displayBusiness(business) })
+   // data.business.forEach(business => { displayBusiness(business) })
     //return data;
-}
+//}
 
-function displayBusiness(business) {
+//function displayBusiness(business) {
 
     //prophet.forEach(prophet => {
         
@@ -117,44 +117,44 @@ function displayBusiness(business) {
 
    
     // Create elements to add to the document
-    let card = document.createElement('section');
-    let h2 = document.createElement('h2');
-    let h3 = document.createElement('h3');
-    let h4 = document.createElement('h4');
-    let h5 = document.createElement('h5');
-    let h6 = document.createElement('h6');
-    let portrait = document.createElement('img');
+    //let card = document.createElement('section');
+    //let h2 = document.createElement('h2');
+    //let h3 = document.createElement('h3');
+    //let h4 = document.createElement('h4');
+    //let h5 = document.createElement('h5');
+    //let h6 = document.createElement('h6');
+    //let portrait = document.createElement('img');
   
     // Change the textContent property of the h2 element to contain the prophet's full name
-    h2.textContent = `${business.name}`;
-    h3.textContent = `${business.address}`;
-    h4.textContent = `${business.phone}`;
-    h5.textContent = `Membership Level: ${business.membership}`;
-    h6.textContent = `${business.website}`;
+    //h2.textContent = `${business.name}`;
+    //h3.textContent = `${business.address}`;
+    //h4.textContent = `${business.phone}`;
+    //h5.textContent = `Membership Level: ${business.membership}`;
+    //h6.textContent = `${business.website}`;
 
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-    portrait.setAttribute('src', business.imageurl);
-    portrait.setAttribute(
-        'alt', 
-        `Portrait of  ${business.name}`
-        );
-    portrait.setAttribute('loading', 'lazy');
+    //portrait.setAttribute('src', business.imageurl);
+    //portrait.setAttribute(
+   //     'alt', 
+    //    `Portrait of  ${business.name}`
+    //    );
+   // portrait.setAttribute('loading', 'lazy');
   
     // Add/append the section(card) with the h2 element
-    card.appendChild(h2);
-    card.appendChild(h3);
-    card.appendChild(h4);
-    card.appendChild(h5);    
-    card.appendChild(h6);
-    card.appendChild(portrait);
+   // card.appendChild(h2);
+   // card.appendChild(h3);
+  //  card.appendChild(h4);
+  //  card.appendChild(h5);    
+  //  card.appendChild(h6);
+  //  card.appendChild(portrait);
     
   
     // Add/append the existing HTML div with the cards class with the section(card)
     //document.querySelector('div.cards').appendChild(card);
-    cards.appendChild(card);
-  }
+ //   cards.appendChild(card);
+//  }
 
-  getBusiness();
+ // getBusiness();
 
 const gridbutton = document.querySelector("#grid");
 const listbutton = document.querySelector("#list");
