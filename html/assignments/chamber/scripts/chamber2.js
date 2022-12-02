e54a3895faabe3189e2610daf9c8219208f99351
bashashhazbaz <ash16016@byui.edu>
2022-12-01 22:37:33 -0600

    week12.10

--- html/assignments/chamber/scripts/chamber2.js
+++ html/assignments/chamber/scripts/chamber2.js
@@ -156,24 +156,24 @@ else {
 
  // getBusiness();
 
-const gridbutton = document.querySelector("#grid");
-const listbutton = document.querySelector("#list");
-const display = document.querySelector("article");
+//const gridbutton = document.querySelector("#grid");
+//const listbutton = document.querySelector("#list");
+//const display = document.querySelector("article");
 
 // The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
 
-gridbutton.addEventListener("click", () => {
+//gridbutton.addEventListener("click", () => {
 	// example using arrow function
-	display.classList.add("grid");
-	display.classList.remove("list");
-});
+	//display.classList.add("grid");
+	//display.classList.remove("list");
+//});
 
-listbutton.addEventListener("click", showList); // example using defined function
+//listbutton.addEventListener("click", showList); // example using defined function
 
-function showList() {
-	display.classList.add("list");
-	display.classList.remove("grid");
-}
+//function showList() {
+	//display.classList.add("list");
+	//display.classList.remove("grid");
+//}
 
 
 
